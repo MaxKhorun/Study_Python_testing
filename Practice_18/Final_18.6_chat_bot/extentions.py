3161bdfc7daf6aca75bf4c842541154254660327
M.Kh <sea-of-max@yandex.ru>
2023-06-18 00:23:15 +0300

    New and Updated

--- Practice_18/Final_18.6_chat_bot/extentions.py
+++ Practice_18/Final_18.6_chat_bot/extentions.py
@@ -10,14 +10,14 @@ class ConverterTool:
             raise APIException(f'Вы ввели одинаковые валюты: "{base} - {quote}"')
 
         try:
-            from_ticker = currency_dict[base]
+            from_ticker = currency_dict[base.lower()]
         except KeyError:
             raise APIException(
                 f'\nНе удалось получить значение введённой валюты: "{base}".\nПроверьте корректность ввода, уточните правильное наименование валюты'\
                 f'по команде: /values.')
 
         try:
-            to_ticker = currency_dict[quote]
+            to_ticker = currency_dict[quote.lower()]
         except KeyError:
             raise APIException(
                 f'Не удалось получить значение введённой валюты: "{quote}".\nПроверьте корректность ввода, уточните правильное наименование валюты'\
@@ -32,6 +32,6 @@ class ConverterTool:
             raise APIException(f'Не удалось обратотать введённое количество валюты для конвертации - "{amount}"')
 
         req_API = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={from_ticker}&tsyms={to_ticker}')
-        result = json.loads(req_API.content)[currency_dict[quote]]
+        result = json.loads(req_API.content)[currency_dict[quote.lower()]]
 
         return result
